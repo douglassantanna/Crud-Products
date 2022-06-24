7be6825670bda5176bdc5e7aa3b4b4c3a022b955
douglas santanna <douglbb1@gmail.com>
2022-06-23 21:51:17 -0300

    implements customer controller

--- services/products.Domain/Itens/DTOs/ViewItem.cs
+++ services/products.Domain/Itens/DTOs/ViewItem.cs
@@ -1,21 +1,20 @@
 using System.Linq.Expressions;
 using products.Domain.Itens.Entities;
 
-namespace products.Domain.Itens.DTOs
+namespace products.Domain.Itens.DTOs;
+
+public class ViewItem
 {
-    public class ViewItem
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+}
+public static class ViewItemExtension
+{
+    public static Expression<Func<Item, ViewItem>> ToView() => x => new ViewItem
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-        public decimal Price { get; set; }
-    }
-    public static class ViewItemExtension
-    {
-        public static Expression<Func<Item, ViewItem>> ToView() => x => new ViewItem
-        {
-            Id = x.Id,
-            Name = x.Name,
-            Price = x.Price
-        };
-    }
+        Id = x.Id,
+        Name = x.Name,
+        Price = x.Price
+    };
 }
